b4e05982c9af2d8097c959ad9eba2cfad8498a6d
Jonathan.Cohen <jonathan.cohen@usta.com>
2019-02-15 19:36:00 -0500

    Added circleci

--- src/main/scala/runner/Main.scala
+++ src/main/scala/runner/Main.scala
@@ -1,9 +1,16 @@
+package runner
+
 import entity.PersonEntity
 import repository.PersonRepository
 
-object Main extends App {
+
+object Main extends Greeting with App {
   val p = PersonRepository.getPerson("Tom")
   println("Found Person: " + p )
   val pe: PersonEntity = p;
   println("Convert Person: " + pe )
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
 }
