1f0bfbd284a68adbf63ec21221e2d645fd83a53a
jcohen66 <jcohen66@optonline.net>
2019-02-18 22:22:47 -0500

    Added case class "update on copy" pattern

---
+++ src/main/scala/caseclass/CopyNested.scala
@@ -0,0 +1,70 @@
+package caseclass
+
+case class BillingInfo(
+                      creditCards: Seq[CreditCard]
+                      )
+
+case class Name(
+               firstName: String,
+               mi: String,
+               lastName: String
+               )
+
+case class User(
+               id: Int,
+               name: Name,
+               billingInfo: BillingInfo,
+               phone: String,
+               email: String
+               )
+
+case class CreditCard(
+                     name: Name,
+                     number: String,
+                     month: Int,
+                     year: Int,
+                     cvv: String
+                     )
+
+object CopyNested extends App {
+
+    val hannahsName = Name(
+      firstName = "Hannah",
+      mi = "C",
+      lastName = "Jones"
+    )
+
+    // create a user
+  val hannah1 = User(
+    id = 1,
+    name = hannahsName,
+    phone = "907-555-1212",
+    email = "hannah@hannahjones.com",
+    billingInfo = BillingInfo(
+      creditCards = Seq(
+        CreditCard(
+          name = hannahsName,
+          number = "1111111111111111",
+          month = 3,
+          year = 2020,
+          cvv = "123"
+        )
+      )
+    )
+  )
+
+  val hannah2 = hannah1.copy(phone = "720-555-1212")
+
+  val newName = hannah2.name.copy(lastName = "Smith")
+
+  val hannah3 = hannah2.copy(name = newName)
+
+  val oldCC = hannah3.billingInfo.creditCards(0)
+
+  val newCC = oldCC.copy(name = newName)
+
+  val newCCs = Seq(newCC)
+
+  val hannah4 = hannah3.copy(billingInfo = BillingInfo(newCCs))
+
+}
