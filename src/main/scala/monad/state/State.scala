4393b60df1dd7e7f9c7f28a7834410606b4f078d
jcohen66 <jcohen66@optonline.net>
2019-02-24 11:10:48 -0500

    Annotated State Monad

--- src/main/scala/monad/state/State.scala
+++ src/main/scala/monad/state/State.scala
@@ -1,6 +1,13 @@
 package monad.state
 
-
+/**
+  * State Monad is s wrapper around state.  Used to "lift" data
+  * out of the wrapper.
+  *
+  * @param  run   FIP used to pass to anonymous function
+  * @tparam S     generic type for state
+  * @tparam A     generic type label
+  */
 case class State[S, A](run: S => (S, A)) {
 
   def flatMap[B](g: A => State[S, B]): State[S, B] = State { (s0: S) =>
@@ -12,7 +19,17 @@ case class State[S, A](run: S => (S, A)) {
 
 }
 
+
 object State {
+
+  /**
+    * lifts (or wraps) a value and a state into ta State[S, A] monad.
+    *
+    * @param v    value
+    * @tparam S   state
+    * @tparam A   type
+    * @return     State[S, A]
+    */
   def point[S, A](v: A): State[S, A] = State(run = s => (s, v))
 }
 
